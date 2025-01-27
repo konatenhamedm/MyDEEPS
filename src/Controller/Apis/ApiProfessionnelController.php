48c2619183ff37d3e3cdbb3963db18bfad376569
konate <konatenhamed@gmail.com>
2025-01-27 16:37:07 +0000

    save update professionel

--- src/Controller/Apis/ApiProfessionnelController.php
+++ src/Controller/Apis/ApiProfessionnelController.php
@@ -5,8 +5,10 @@ namespace App\Controller\Apis;
 
 use App\Controller\Apis\Config\ApiInterface;
 use App\DTO\ProfessionnelDTO;
+use App\Entity\Organisation;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use App\Entity\Professionnel;
+use App\Repository\OrganisationRepository;
 use App\Repository\ProfessionnelRepository;
 use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,7 +21,7 @@ use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 
 class ApiProfessionnelController extends ApiInterface
 {
-    
+
 
     #[Route('/', methods: ['GET'])]
     /**
@@ -101,20 +103,176 @@ class ApiProfessionnelController extends ApiInterface
     #[OA\Post(
         summary: "Creation de professionnel",
         description: "Permet de crtéer un professionnel.",
+
         requestBody: new OA\RequestBody(
             required: true,
-            content: new OA\JsonContent(
-                properties: [
-                    new OA\Property(property: "user", type: "string"),// user
+            content: new OA\MediaType(
+                mediaType: "multipart/form-data",
+                schema: new OA\Schema(
+                    properties: [
+                        new OA\Property(property: "user", type: "string"), // user
+                        new OA\Property(property: "numero", type: "string"), // code_verification ..
+                        new OA\Property(property: "nom", type: "string"), //first_name 
+                        new OA\Property(property: "prenoms", type: "string"),
+                        new OA\Property(property: "emailPro", type: "string"), //email_pro
+                        new OA\Property(property: "address", type: "string"), //address
+                        new OA\Property(property: "professionnel", type: "string"), //professionnel
+                        new OA\Property(property: "addressPro", type: "string"), //address_pro
 
-                    new OA\Property(property: "numero", type: "string"),// code_verification ..
-                    new OA\Property(property: "nom", type: "string"),//first_name 
+                        new OA\Property(property: "profession", type: "string"), //profession
+                        new OA\Property(property: "civilite", type: "string"), //civilite
+                        new OA\Property(property: "adresseEmail", type: "string"), //adresseEmail
+                        new OA\Property(property: "dateDiplome", type: "string"), //dateDiplome
+                        new OA\Property(property: "dateNaissance", type: "string"), //dateNaissance
+                        new OA\Property(property: "contactPro", type: "string"), //contactPerso
+                        new OA\Property(property: "dateEmploi", type: "string"), //dateEmploi
+                        new OA\Property(property: "nationate", type: "string"), //nationate
+                        new OA\Property(property: "diplome", type: "string"), //diplome
+                        new OA\Property(property: "situationPro", type: "string"), //situation_pro
+
+
+                        new OA\Property(property: "photo", type: "string"), //photo
+                        new OA\Property(property: "cni", type: "string"), //cni
+                        new OA\Property(property: "casier", type: "string"), //casier
+                        new OA\Property(property: "diplomeFile", type: "string"), //diplomeFile
+                        new OA\Property(property: "certificat", type: "string"), //certificat
+                        new OA\Property(property: "cv", type: "string"), //cv
+                        new OA\Property(property: "appartenirOrganisation", type: "string"), //cv
+                        new OA\Property(property: "userUpdate", type: "string"),
+
+
+                    ],
+                    type: "object"
+                )
+            )
+
+        ),
+
+
+        responses: [
+            new OA\Response(response: 401, description: "Invalid credentials")
+        ]
+    )]
+    #[OA\Tag(name: 'professionnel')]
+    #[Security(name: 'Bearer')]
+    public function create(Request $request, ProfessionnelRepository $professionnelRepository, OrganisationRepository $organisationRepository): Response
+    {
+
+        $names = 'document_' . '01';
+        $filePrefix  = str_slug($names);
+        $filePath = $this->getUploadDir(self::UPLOAD_PATH, true);
+       
+
+        $professionnel = new Professionnel();
+
+        $professionnel->setNumber($request->get('numero'));
+        $professionnel->setNom($request->get('nom'));
+        $professionnel->setPrenoms($request->get('prenoms'));
+        $professionnel->setEmailPro($request->get('emailPro'));
+        $professionnel->setAddress($request->get('address'));
+        $professionnel->setProfessionnel($request->get('professionnel'));
+        $professionnel->setAddressPro($request->get('addressPro'));
+        $professionnel->setProfession($request->get('professionnel'));
+        $professionnel->setCivilite($request->get('civilite'));
+        $professionnel->setAdresseEmail($request->get('adresseEmail'));
+        $professionnel->setDateDiplome($request->get('dateDiplome'));
+        $professionnel->setDateNaissance($request->get('dateNaissance'));
+        $professionnel->setContactPro($request->get('contactPro'));
+
+        $professionnel->setDateEmploi($request->get('dateEmploi'));
+        $professionnel->setNationate($request->get('nationate'));
+        $professionnel->setDiplome($request->get('diplome'));
+        $professionnel->setSituationPro($request->get('situationPro'));
+
+
+        $uploadedPhoto = $request->files->get('photo');
+        $uploadedCasier = $request->files->get('caiser');
+        $uploadedCni = $request->files->get('cni');
+        $uploadedDiplome = $request->files->get('diplomeFile');
+        $uploadedCertificat = $request->files->get('certificat');
+        $uploadedCv = $request->files->get('cv');
+
+        if ($uploadedPhoto) {
+            $fichier = $this->utils->sauvegardeFichier($filePath, $filePrefix, $uploadedPhoto, self::UPLOAD_PATH);
+            if ($fichier) {
+                $professionnel->setPhoto($fichier);
+            }
+        }
+        if ($uploadedCasier) {
+            $fichier = $this->utils->sauvegardeFichier($filePath, $filePrefix, $uploadedCasier, self::UPLOAD_PATH);
+            if ($fichier) {
+                $professionnel->setCasier($fichier);
+            }
+        }
+        if ($uploadedCni) {
+            $fichier = $this->utils->sauvegardeFichier($filePath, $filePrefix, $uploadedCni, self::UPLOAD_PATH);
+            if ($fichier) {
+                $professionnel->setCni($fichier);
+            }
+        }
+        if ($uploadedDiplome) {
+            $fichier = $this->utils->sauvegardeFichier($filePath, $filePrefix, $uploadedDiplome, self::UPLOAD_PATH);
+            if ($fichier) {
+                $professionnel->setDiplomeFile($fichier);
+            }
+        }
+        if ($uploadedCertificat) {
+            $fichier = $this->utils->sauvegardeFichier($filePath, $filePrefix, $uploadedCertificat, self::UPLOAD_PATH);
+            if ($fichier) {
+                $professionnel->setCertificat($fichier);
+            }
+        }
+        if ($uploadedCv) {
+            $fichier = $this->utils->sauvegardeFichier($filePath, $filePrefix, $uploadedCv, self::UPLOAD_PATH);
+            if ($fichier) {
+                $professionnel->setCv($fichier);
+            }
+        }
+
+        $professionnel->setAppartenirOrganisation($request->get('appartenirOrganisation'));
+        $professionnel->setUser($this->userRepository->find($request->get('user')));
+
+
+        $professionnel->setCreatedBy($this->userRepository->find($request->get('userUpdate')));
+        $professionnel->setUpdatedBy($this->userRepository->find($request->get('userUpdate')));
+
+        $errorResponse = $this->errorResponse($professionnel);
+        if ($errorResponse !== null) {
+            return $errorResponse; // Retourne la réponse d'erreur si des erreurs sont présentes
+        } else {
+            $professionnelRepository->add($professionnel, true);
+
+            if ($professionnel->getAppartenirOrganisation() == "oui") {
+
+                $organisation = new Organisation();
+                $organisation->setNom("");
+                $organisation->setAnnee("");
+                $organisation->setEntite($professionnel);
+                $organisation->setCreatedBy($this->userRepository->find($request->get('userUpdate')));
+                $organisation->setUpdatedBy($this->userRepository->find($request->get('userUpdate')));
+                $organisationRepository->add($organisation, true);
+            }
+        }
+        return $this->responseData($professionnel, 'group_pro', ['Content-Type' => 'application/json']);
+    }
+
+
+    #[Route('/update/{id}', methods: ['PUT', 'POST'])]
+    #[OA\Post(
+        summary: "Creation de professionnel",
+        description: "Permet de créer un professionnel.",
+        content: new OA\MediaType(
+            mediaType: "multipart/form-data",
+            schema: new OA\Schema(
+                properties: [
+                    new OA\Property(property: "user", type: "string"), // user
+                    new OA\Property(property: "numero", type: "string"), // code_verification ..
+                    new OA\Property(property: "nom", type: "string"), //first_name 
                     new OA\Property(property: "prenoms", type: "string"),
                     new OA\Property(property: "emailPro", type: "string"), //email_pro
                     new OA\Property(property: "address", type: "string"), //address
                     new OA\Property(property: "professionnel", type: "string"), //professionnel
                     new OA\Property(property: "addressPro", type: "string"), //address_pro
-
 
                     new OA\Property(property: "profession", type: "string"), //profession
                     new OA\Property(property: "civilite", type: "string"), //civilite
@@ -141,49 +299,7 @@ class ApiProfessionnelController extends ApiInterface
                 ],
                 type: "object"
             )
-        ),
-        responses: [
-            new OA\Response(response: 401, description: "Invalid credentials")
-        ]
-    )]
-    #[OA\Tag(name: 'professionnel')]
-    #[Security(name: 'Bearer')]
-    public function create(Request $request, ProfessionnelRepository $professionnelRepository): Response
-    {
-
-        $data = json_decode($request->getContent(), true);
-        $professionnel = new Professionnel();
-       
-        $professionnel->setProfessionnel($data['professionnel']);
-        $professionnel->setCreatedBy($this->userRepository->find($data['sender']));
-        $professionnel->setUpdatedBy($this->userRepository->find($data['sender']));
-        $errorResponse = $this->errorResponse($professionnel);
-        if ($errorResponse !== null) {
-            return $errorResponse; // Retourne la réponse d'erreur si des erreurs sont présentes
-        } else {
-            $professionnelRepository->add($professionnel, true);
-        }
-        return $this->responseData($professionnel, 'group_pro', ['Content-Type' => 'application/json']);
-    }
-
-
-    #[Route('/update/{id}', methods: ['PUT', 'POST'])]
-    #[OA\Post(
-        summary: "Creation de professionnel",
-        description: "Permet de créer un professionnel.",
-        requestBody: new OA\RequestBody(
-            required: true,
-            content: new OA\JsonContent(
-                properties: [
-                    new OA\Property(property: "sender", type: "string"),
-                    new OA\Property(property: "receiver", type: "string"),
-                    new OA\Property(property: "professionnel", type: "string"),
-                    new OA\Property(property: "userUpdate", type: "string"),
-
-                ],
-                type: "object"
-            )
-        ),
+            ),
         responses: [
             new OA\Response(response: 401, description: "Invalid credentials")
         ]
@@ -193,31 +309,92 @@ class ApiProfessionnelController extends ApiInterface
     public function update(Request $request, Professionnel $professionnel, ProfessionnelRepository $professionnelRepository): Response
     {
         try {
-            $data = json_decode($request->getContent());
-            if ($professionnel != null) {
+            $names = 'document_' . '01';
+            $filePrefix  = str_slug($names);
+            $filePath = $this->getUploadDir(self::UPLOAD_PATH, true);
+           
 
-                $professionnel->setSender($this->userRepository->find($data->sender));
-                $professionnel->setSender($this->userRepository->find($data->receiver));
-                $professionnel->setProfessionnel($data->professionnel);
-                $professionnel->setUpdatedBy($this->userRepository->find($data->userUpdate));
-                $professionnel->setUpdatedAt(new \DateTime());
+            if($professionnel){
+                $professionnel->setNumber($request->get('numero'));
+                $professionnel->setNom($request->get('nom'));
+                $professionnel->setPrenoms($request->get('prenoms'));
+                $professionnel->setEmailPro($request->get('emailPro'));
+                $professionnel->setAddress($request->get('address'));
+                $professionnel->setProfessionnel($request->get('professionnel'));
+                $professionnel->setAddressPro($request->get('addressPro'));
+                $professionnel->setProfession($request->get('professionnel'));
+                $professionnel->setCivilite($request->get('civilite'));
+                $professionnel->setAdresseEmail($request->get('adresseEmail'));
+                $professionnel->setDateDiplome($request->get('dateDiplome'));
+                $professionnel->setDateNaissance($request->get('dateNaissance'));
+                $professionnel->setContactPro($request->get('contactPro'));
+        
+                $professionnel->setDateEmploi($request->get('dateEmploi'));
+                $professionnel->setNationate($request->get('nationate'));
+                $professionnel->setDiplome($request->get('diplome'));
+                $professionnel->setSituationPro($request->get('situationPro'));
+        
+        
+                $uploadedPhoto = $request->files->get('photo');
+                $uploadedCasier = $request->files->get('caiser');
+                $uploadedCni = $request->files->get('cni');
+                $uploadedDiplome = $request->files->get('diplomeFile');
+                $uploadedCertificat = $request->files->get('certificat');
+                $uploadedCv = $request->files->get('cv');
+        
+                if ($uploadedPhoto) {
+                    $fichier = $this->utils->sauvegardeFichier($filePath, $filePrefix, $uploadedPhoto, self::UPLOAD_PATH);
+                    if ($fichier) {
+                        $professionnel->setPhoto($fichier);
+                    }
+                }
+                if ($uploadedCasier) {
+                    $fichier = $this->utils->sauvegardeFichier($filePath, $filePrefix, $uploadedCasier, self::UPLOAD_PATH);
+                    if ($fichier) {
+                        $professionnel->setCasier($fichier);
+                    }
+                }
+                if ($uploadedCni) {
+                    $fichier = $this->utils->sauvegardeFichier($filePath, $filePrefix, $uploadedCni, self::UPLOAD_PATH);
+                    if ($fichier) {
+                        $professionnel->setCni($fichier);
+                    }
+                }
+                if ($uploadedDiplome) {
+                    $fichier = $this->utils->sauvegardeFichier($filePath, $filePrefix, $uploadedDiplome, self::UPLOAD_PATH);
+                    if ($fichier) {
+                        $professionnel->setDiplomeFile($fichier);
+                    }
+                }
+                if ($uploadedCertificat) {
+                    $fichier = $this->utils->sauvegardeFichier($filePath, $filePrefix, $uploadedCertificat, self::UPLOAD_PATH);
+                    if ($fichier) {
+                        $professionnel->setCertificat($fichier);
+                    }
+                }
+                if ($uploadedCv) {
+                    $fichier = $this->utils->sauvegardeFichier($filePath, $filePrefix, $uploadedCv, self::UPLOAD_PATH);
+                    if ($fichier) {
+                        $professionnel->setCv($fichier);
+                    }
+                }
+        
+                $professionnel->setAppartenirOrganisation($request->get('appartenirOrganisation'));
+                $professionnel->setUser($this->userRepository->find($request->get('user')));
+        
+        
+                $professionnel->setCreatedBy($this->userRepository->find($request->get('userUpdate')));
+                $professionnel->setUpdatedBy($this->userRepository->find($request->get('userUpdate')));
+        
                 $errorResponse = $this->errorResponse($professionnel);
-
                 if ($errorResponse !== null) {
                     return $errorResponse; // Retourne la réponse d'erreur si des erreurs sont présentes
                 } else {
                     $professionnelRepository->add($professionnel, true);
                 }
-
-
-
-                // On retourne la confirmation
-                $response = $this->responseData($professionnel, 'group1', ['Content-Type' => 'application/json']);
-            } else {
-                $this->setMessage("Cette ressource est inexsitante");
-                $this->setStatusCode(300);
-                $response = $this->response('[]');
+                $response = $this->responseData($professionnel, 'group_pro', ['Content-Type' => 'application/json']);
             }
+            
         } catch (\Exception $exception) {
             $this->setMessage("");
             $response = $this->response('[]');
@@ -298,5 +475,4 @@ class ApiProfessionnelController extends ApiInterface
         }
         return $response;
     }
-
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -133,6 +133,12 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\OneToMany(targetEntity: Message::class, mappedBy: 'sender')]
     private Collection $messages;
 
+    /**
+     * @var Collection<int, Transaction>
+     */
+    #[ORM\OneToMany(targetEntity: Transaction::class, mappedBy: 'user')]
+    private Collection $transactions;
+
 
     public function __construct()
     {
@@ -148,7 +154,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
             $this->payement = "init_payement"; // payed payed-inifinty
 
         }
-        $this->messages = new ArrayCollection(); 
+        $this->messages = new ArrayCollection();
+        $this->transactions = new ArrayCollection(); 
     }
 
 
@@ -454,6 +461,36 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
             // set the owning side to null (unless already changed)
             if ($message->getSender() === $this) {
                 $message->setSender(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Transaction>
+     */
+    public function getTransactions(): Collection
+    {
+        return $this->transactions;
+    }
+
+    public function addTransaction(Transaction $transaction): static
+    {
+        if (!$this->transactions->contains($transaction)) {
+            $this->transactions->add($transaction);
+            $transaction->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeTransaction(Transaction $transaction): static
+    {
+        if ($this->transactions->removeElement($transaction)) {
+            // set the owning side to null (unless already changed)
+            if ($transaction->getUser() === $this) {
+                $transaction->setUser(null);
             }
         }
 
