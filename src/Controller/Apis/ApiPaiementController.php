bdbe02258bd32fab2ec8792f4b9dc37445d195b6
konatenhamedm <konatenhamed@gmail.com>
2025-09-14 23:41:57 +0000

    save

--- src/Controller/Apis/ApiPaiementController.php
+++ src/Controller/Apis/ApiPaiementController.php
@@ -721,7 +721,7 @@ class ApiPaiementController extends ApiInterface
             $documents = $request->get('documents');
             $uploadedFiles = $request->files->get('documents');
 
-            dd($request->files->get('documents'));
+            dd($request->get('documents'));
 
             foreach ($documents as $index => $doc) {
 
