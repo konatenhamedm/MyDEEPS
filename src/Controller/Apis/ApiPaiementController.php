ebb31580952baf8ad96766cf5748ad41517d8b1d
konatenhamedm <konatenhamed@gmail.com>
2025-09-14 10:20:50 +0000

    save

--- src/Controller/Apis/ApiPaiementController.php
+++ src/Controller/Apis/ApiPaiementController.php
@@ -161,7 +161,7 @@ class ApiPaiementController extends ApiInterface
                 ['user' => $userId, 'state' => 1],
                 ['createdAt' => 'DESC']
             );
-            if ($user->getType() != "professionnel") {
+            if ($user->getType() == "PROFESSIONNEL") {
                 $profession = $professionRepository->findOneByCode($user->getPersonne()->getProfession());
 
 
