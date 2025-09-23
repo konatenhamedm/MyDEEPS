10d8b296c095e8695560350a1318c0d3faae1af9
konatenhamedm <konatenhamed@gmail.com>
2025-09-23 06:07:40 +0000

    ssave

--- src/Controller/Apis/ApiProfessionnelController.php
+++ src/Controller/Apis/ApiProfessionnelController.php
@@ -338,7 +338,7 @@ class ApiProfessionnelController extends ApiInterface
             $professionnels = $userRepository->findActiveProfessionnelsByImputationWithouParam();
             //$professionnels = $userRepository->findBy(['typeUser' => 'PROFESSIONNEL'], ['id' => 'DESC']);
 
-            dd($professionnels);
+          
 
             $formattedProfessionnels = array_map(function ($professionnel) use ($professionRepository) {
                 $personne = $professionnel->getPersonne();
@@ -412,6 +412,7 @@ class ApiProfessionnelController extends ApiInterface
         } catch (\Exception $exception) {
             $this->setMessage("");
             $this->response('[]');
+            $response = $this->response('[]');
         }
 
         // On envoie la réponse
--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -73,7 +73,7 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
             ->andWhere('u.typeUser = :type')
             ->andWhere('u.actived = :active')
             ->setParameter('type', 'PROFESSIONNEL')
-            ->setParameter('active', true)
+            ->setParameter('active', 1)
             ->orderBy('u.id', 'DESC')
             ->getQuery()
             ->getResult();
