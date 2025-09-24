384edeca9985beebbf0f2e2540376844850fd4f1
konatenhamedm <konatenhamed@gmail.com>
2025-09-24 21:31:15 +0000

    save

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -105,7 +105,7 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
         return $this->createQueryBuilder('u')
             ->andWhere('u.roles LIKE :role')
             /* ->andWhere('u.typeUser = :typeUser') */
-             ->andWhere('u.deleteAt IS NOT NULL')
+             ->andWhere('u.deleteAt IS  NULL')
             ->setParameter('role', '%"ROLE_ADMIN"%')
             /*  ->setParameter('typeUser', 'ADMINISTRATEUR') */
             ->getQuery()
@@ -115,7 +115,7 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
     {
         return $this->createQueryBuilder('u')
             ->andWhere('u.roles NOT LIKE :role')
-            ->andWhere('u.deleteAt IS NOT NULL')
+            ->andWhere('u.deleteAt IS  NULL')
             // ->andWhere('u.typeUser = :typeUser') // si besoin
             ->setParameter('role', '%"ROLE_ADMIN"%')
             // ->setParameter('typeUser', 'ADMINISTRATEUR')
