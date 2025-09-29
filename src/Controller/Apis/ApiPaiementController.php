e4ad72ff834f9477bcf0e33e637d97a55458fd6f
konatenhamedm <konatenhamed@gmail.com>
2025-09-29 11:50:57 +0000

    save

--- src/Controller/Apis/ApiPaiementController.php
+++ src/Controller/Apis/ApiPaiementController.php
@@ -91,7 +91,7 @@ class ApiPaiementController extends ApiInterface
             $formattedTransactions = array_map(function (Transaction $transaction) use ($professionRepository, $type) {
                 $personne = $transaction->getUser()->getPersonne();
                 $profession =  $type == "professionnel" ? $personne->getProfession() ? $professionRepository->findOneByCode($personne->getProfession()) : null : null;
-                dd($personne);
+                //dd($personne);
                 return [
                     "montant" => $transaction->getMontant(),
                     "reference" => $transaction->getReference(),
@@ -123,9 +123,9 @@ class ApiPaiementController extends ApiInterface
                     ] : [
                         "code" => $personne->getCode(),
                         "email" => $personne->getEmail(),
-                        "nom" => $personne->getTypeSociete()->getLibelle() == "PHYSIQUE" ? $personne->getNom() : "",
-                        "denomination" => $personne->getTypeSociete()->getLibelle() == "MORALE" ? $personne->getDenomination() : "",
-                        "prenoms" => $personne->getTypeSociete()->getLibelle() == "PHYSIQUE" ? $personne->getPrenoms() : "",
+                        "nom" => $personne->getTypePersonne()->getLibelle() == "PHYSIQUE" ? $personne->getNom() : "",
+                        "denomination" => $personne->getTypePersonne()->getLibelle() == "MORALE" ? $personne->getDenomination() : "",
+                        "prenoms" => $personne->getTypePersonne()->getLibelle() == "PHYSIQUE" ? $personne->getPrenoms() : "",
                         "createdAt" => $personne->getCreatedAt()->format('Y-m-d H:i:s')
                     ],
 
